f4bbb68b55d945878e07c27596ac247af5cfb0b6
Agustin800 <aguslahal@gmail.com>
2025-02-19 16:47:17 +0000

    print numberz 1st

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - do things
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar('\n');
+	}
+
+	return (0);
+}
