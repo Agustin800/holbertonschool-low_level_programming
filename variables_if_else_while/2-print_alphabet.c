673e8084c4f81f2eb50d7d053ea8e72f6ff41585
Agustin800 <aguslahal@gmail.com>
2025-02-19 14:52:32 +0000

    print alphabet 1st

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letra;
+
+		for (letra = 'a'; letra <= 'z'; letra++)
+	{
+		putchar(letra);
+	}
+	{
+			putchar('\n');
+	}
+
+		return (0);
+}
