dd73c464771aa9027570541b73adf2e34fcda890
Agustin800 <aguslahal@gmail.com>
2025-02-19 15:49:27 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - do things with numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		{
+
+		printf("%d", n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
