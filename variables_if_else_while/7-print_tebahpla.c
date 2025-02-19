3a2e693c8e47ad0b027f2238825f8aa507c2852f
Agustin800 <aguslahal@gmail.com>
2025-02-19 16:56:51 +0000

    print tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - do thing
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letra;
+
+		for (letra = 'z'; letra <= 'a'; letra++)
+	{
+		putchar(letra);
+	}
+	{
+			putchar('\n');
+	}
+
+		return (0);
+}
