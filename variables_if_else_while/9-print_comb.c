d15aa267bf67306e90ebcc63357b7b917c435f44
Agustin800 <aguslahalo@gmial.com>
2025-02-20 14:01:36 +0000

    print comb 3st

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - do things
- * Return: 0
+ *main - do things
+ *Return: 0
  */
 
 int main(void)
 {
-	char n;
-
-	for (n = '0'; n <= '9'; n++)
-	{
-		putchar(n);
-		if (n != '9')
-		putchar (',');
-		putchar (' ');
-	}
-	{
-		putchar('\n');
-	}
-
+int n;
+for (n = '0'; n <= '9'; c++)
+{
+putchar(n);
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 	return (0);
 }
