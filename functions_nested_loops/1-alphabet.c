edbec09cb88e438fef53eee88145c1d5fe0373d6
Agustin800 <aguslahalo@gmial.com>
2025-02-20 17:16:44 +0000

    alphabet 1st

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - imprime las letras de la a a la z
+ */
+void print_alphabet(void)
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+	{
+		_putchar(letra);
+	}
+		_putchar(10);
+}
