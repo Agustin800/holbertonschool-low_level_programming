aa248d0ae07e3c71c8b1c3690e721fc237ba059f
Agustin800 <aguslahalo@gmail.com>
2025-02-21 17:21:27 +0000

    24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *jack_bauer - Prints every minute of the day of Jack Bauer.
+ *
+ * Return: No return value.
+ */
+
+void jack_bauer(void)
+{
+	char h1 = 0, h2 = 0, h3 = 0, m1 = 0, m2 = 0;
+
+	while (h3 < 3)
+	{
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(m1 + '0');
+		_putchar(m2 + '0');
+		_putchar('\n');
+
+		m2++;
+
+		if (m2 > 9)
+		{
+			m1++;
+			m2 = 0;
+		}
+		if (m1 > 5)
+		{
+			h2++;
+			m2 = 0;
+		}
+		if (h2 > 9)
+		{
+			h1++;
+			h2 = 0;
+		}
+		if (h1 == 2 && h2 > 3)
+			h3 = 3;
+	}
+}
