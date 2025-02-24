8d4eda02191d73a8a56f8ee4f1081b3f8067ad27
Agustin800 <aguslahalo@gmail.com>
2025-02-24 16:07:21 +0000

    print more numbers 2nd

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,7 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
-	{
-		if (n != 2 && n != 4)
-		_putchar('0' + n);
-		_putchar('\n');
-	}
+	if (n != 2 && n != 4)
+	_putchar('0' + n);
+	_putchar('\n');
 }
