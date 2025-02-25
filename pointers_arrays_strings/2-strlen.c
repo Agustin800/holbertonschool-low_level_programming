1aceb9b539f611053ec51cf00475da2bf70ab5b8
Agustin800 <aguslahalo@gmail.com>
2025-02-25 16:17:57 +0000

    stralen nd

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,18 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen - do things
  *Return: length of a string.
- *@s: pointer
+ *@s: pointer.
  */
 
 int _strlen(char *s)
 {
-	return (strlen(s));
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
 }
