68fe96895863147dbd448c2119fe5e4d95cedd46
Agustin800 <aguslahalo@gmail.com>
2025-02-27 13:00:14 +0000

    puts 1st

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *puts2 - do things
+ *@str: pointer
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a += 2;
+	}
+	_putchar('\n');
+}
