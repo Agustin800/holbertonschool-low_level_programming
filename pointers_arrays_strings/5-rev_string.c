63f9410a2e28e19607665abfbd83635e1f769da1
Agustin800 <aguslahalo@gmail.com>
2025-02-27 12:44:30 +0000

    test

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
- * 
+ *rev_string - do things
+ *@s: pointer
  */
 
- void rev_string(char *s)
- {
-    
- }
+void rev_string(char *s)
+{
+	int a = 0, i;
+	char temp;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for  (i = 0; i < a / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[a - 1 - i];
+		s[a - 1 - i] = temp;
+	}
+}
