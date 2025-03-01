c090e312c79d72ad847d5be9bc2f91aff022af0f
Agustin800 <aguslahalo@gmail.com>
2025-03-01 13:52:48 +0000

    strcmp 1st

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcmp - do things
+ *@s1: char 1
+ *@s2: char 2
+ *Return: positive if s1 < s2, negative if s2 > s1, 0 if s1 = s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s1[i]);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
