5863188aeffc34f5cfa53e576b4c6999f3abce6c
Agustin800 <aguslahalo@gmail.com>
2025-03-01 14:53:56 +0000

    string toupper 1st

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ **string_toupper - do thigs
+ *@a:pointer
+ *Return:Pointer to the modified string.
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
