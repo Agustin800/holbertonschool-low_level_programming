67ba6d0870a83dba63fbe7e9aedfbd4386d5c260
Agustin800 <aguslahalo@gmail.com>
2025-03-01 14:05:58 +0000

    rev array 1st

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *reverse_array - do things
+ *@a: int 1
+ *@n: int 2
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
