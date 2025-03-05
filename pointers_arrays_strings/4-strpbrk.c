2a7bac636a3c0e944e4e65909cc86fc048f0a07c
Agustin800 <aguslahalo@gmail.com>
2025-03-05 17:06:44 +0000

    strpbrk 1st

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strpbrk - do things
+ *@s:the string to search
+ *@accept:the bytes to search for
+ *Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (s);
+}
