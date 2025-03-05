13b796acc8f016d2edf3d869f2f20afb316f5004
Agustin800 <aguslahalo@gmail.com>
2025-03-05 17:14:17 +0000

    preludeof strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - do things
+ *@haystack: the string to search
+ *@needle: the substring to search for
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	
+}
