5b15a16f00a589bd1430546416b950bf92c6d837
Agustin800 <aguslahalo@gmail.com>
2025-03-09 19:56:56 +0000

    add 1st

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Adds positive numbers and prints the result
+ * @argc: The number of arguments
+ * @argv: The argument vector
+ * Return: 0 if successful, 1 if there is a non-digit argumen
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
