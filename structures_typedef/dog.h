5d4e7e9767e10a7ee8cf314661e64cf09b00b407
Agustin800 <aguslahalo@gmail.com>
2025-03-16 18:41:51 +0000

    i really hate betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - do things
+ * @name: Pointer to dog's name (string)
+ * @age: Dog's age (float)
+ * @owner: Pointer to owner's name (string)
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+
+#endif
