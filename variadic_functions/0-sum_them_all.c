4e557ada4a8bfd3f44e8160725bd8c75c707d571
Agustin800 <aguslahalo@gmail.com>
2025-03-23 21:52:59 +0000

    test push

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_ all - do things
+ * @n: numbers
+ * Return: 0 if n == 0 else return n
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list = va_args;
+	int i;
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 void print_all(const char * const format, ...);
+int sum_them_all(const unsigned int n, ...);
 
