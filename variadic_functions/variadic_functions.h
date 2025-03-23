1f7cba583196edd9b51937a306d3a71fb355d004
Agustin800 <aguslahalo@gmail.com>
2025-03-23 22:05:28 +0000

    header betty fix

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 int _putchar(char c);
 void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 
+#endif
